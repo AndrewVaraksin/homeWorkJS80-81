ceae0483cf4fe7260113560518880715e8732ff7
AndrewVaraksin <136505658+AndrewVaraksin@users.noreply.github.com>
2023-08-10 23:38:16 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+// =================FOR / WHILE================
+
+// for(i = 2; i <= 12; i = i + 2){
+//   console.log(i)
+// }
+
+// for(i = 10; i >= 1; i = i - 1){
+//   console.log(i)
+// }
+
+
+// let i = 2
+// while (i < 12) {
+//   i = i + 2
+//   console.log(i)
+// }
+
+// let i = 11
+// while (i > 1) {
+//   i = i - 1
+//   console.log(i)
+// }
+
+// =================МАССИВ================
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 20, 22]
+
+// for(let i = 0; i < numbers.length; i++){
+//   if(numbers[i] % 2 == 0)
+//   console.log(numbers[i])
+// }
+
+let color = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+for(let i = color.length - 1; i >= 0; i--){
+  
+  console.log(color[i])
+
+}
